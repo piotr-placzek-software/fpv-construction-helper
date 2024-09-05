f02402f8267d3c139092909cbd02de83d745321d
Piotr Płaczek <piotr.placzek.91@gmail.com>
2024-09-05 16:19:52 +0200

    (#6) organize core modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -7,9 +6,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { AppConfigurationModule } from './core-modules/configuration/app-configuration.module';
+import { AppCoreModule } from './core-modules/app-core.module';
 import { AppNavigationItem } from './core-modules/navigation/app-navigation.item';
-import { AppNavigationModule } from './core-modules/navigation/app-navigation.module';
 import { AppNavigationService } from './core-modules/navigation/app-navigation.service';
 import { AppCalculatorModule } from './features-modules/calculator/app-calculator.module';
 import { AppLayoutComponent } from './layout/app-layout.component';
@@ -17,7 +15,6 @@ import { AppSideNavComponent } from './layout/side-nav/app-side-nav.component';
 import { AppToolbarComponent } from './layout/toolbar/app-toolbar.component';
 import { AppHomePageComponent } from './pages/home-page/app-home-page.component';
 
-const AppCoreModules = [AppConfigurationModule, AppNavigationModule];
 const AppFeatureModules = [AppCalculatorModule];
 const MatModules = [MatToolbarModule, MatSidenavModule, MatCheckboxModule];
 const AppLayoutComponents = [AppLayoutComponent, AppToolbarComponent, AppSideNavComponent];
@@ -27,9 +24,8 @@ const AppLayoutComponents = [AppLayoutComponent, AppToolbarComponent, AppSideNav
         BrowserModule,
         BrowserAnimationsModule,
         RouterModule.forRoot([{ path: '', redirectTo: 'home', pathMatch: 'full' }]),
-        FormsModule,
         ...MatModules,
-        ...AppCoreModules,
+        AppCoreModule,
         ...AppFeatureModules,
     ],
     bootstrap: [AppComponent],
--- src/app/features-modules/calculator/app-calculator.module.ts
+++ src/app/features-modules/calculator/app-calculator.module.ts
@@ -4,22 +4,20 @@ import { MatCardModule } from '@angular/material/card';
 import { MatOptionModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
-import { AppConfigurationModule } from '../../core-modules/configuration/app-configuration.module';
+import { AppCoreModule } from '../../core-modules/app-core.module';
 import { AppConfigurationService } from '../../core-modules/configuration/app-configuration.service';
 import { AppNavigationItem } from '../../core-modules/navigation/app-navigation.item';
-import { AppNavigationModule } from '../../core-modules/navigation/app-navigation.module';
 import { AppNavigationService } from '../../core-modules/navigation/app-navigation.service';
 import { AppCalculatorPage } from './app-calculator.page';
 import { AppCalculatorService } from './app-calculator.service';
 import { AppKvCalculatorComponent } from './kv-calculator/app-kv-calculator.component';
-import { CommonModule } from '@angular/common';
 
 const AppCalculatorComponents = [AppKvCalculatorComponent];
 const MatModules = [MatCardModule, MatFormFieldModule, MatSelectModule, MatOptionModule];
 
 @NgModule({
     declarations: [AppCalculatorPage, ...AppCalculatorComponents],
-    imports: [CommonModule, FormsModule, AppConfigurationModule, AppNavigationModule, ...MatModules],
+    imports: [FormsModule, AppCoreModule, ...MatModules],
     providers: [AppCalculatorService],
 })
 export class AppCalculatorModule {
