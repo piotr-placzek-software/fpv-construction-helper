8acc16b1356709931bf32f4c42bddd47043ae351
Piotr Płaczek <piotr.placzek.91@gmail.com>
2024-09-07 14:39:27 +0200

    (#10) add pts to secondary rpm converter

--- src/app/features-modules/calculator/calculators/sfm-converter/app-sfm-converter-calculator.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-sfm-converter-calculator',
-    templateUrl: './app-sfm-converter-calculator.component.html',
-})
-export class AppSfmConverterCalculatorComponent {}
