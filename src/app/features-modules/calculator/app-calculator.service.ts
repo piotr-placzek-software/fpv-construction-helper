b52814fa54ea6989e2efd5197c1ee9b53ac36751
Piotr Płaczek <piotr.placzek.91@gmail.com>
2024-09-07 13:53:52 +0200

    (#10) add some small improvements and thanks on home page

--- src/app/features-modules/calculator/app-calculator.service.ts
+++ src/app/features-modules/calculator/app-calculator.service.ts
@@ -3,24 +3,33 @@ import { BatteryVmax, PropellerSize } from '../../shared/types/fpv-specific.type
 
 @Injectable()
 export class AppCalculatorService {
-    calculateKv(batterySize: BatteryVmax, propellerSize: PropellerSize): number {
-        return Math.round(720000 / (propellerSize * 3.14 * batterySize));
+    public calculateKv(batterySize: BatteryVmax, propellerSize: PropellerSize): number {
+        return this.round(720000 / (propellerSize * 3.14 * batterySize), 0);
     }
 
-    calculateRpm(batterySize: BatteryVmax, kv: number, loses = 0): number {
+    public calculateRpm(batterySize: BatteryVmax, kv: number, loses = 0): number {
         if (loses < 0 || loses > 1) {
             throw new Error('Loses value should be [0,1]');
         }
-        return Math.round(batterySize * kv * (1 - loses));
+        return this.round(batterySize * kv * (1 - loses));
     }
 
-    calculatePropellerTipSpeed(batterySize: BatteryVmax, propellerSize: PropellerSize, kv: number, loses = 0): number {
+    public calculatePropellerTipSpeed(
+        batterySize: BatteryVmax,
+        propellerSize: PropellerSize,
+        kv: number,
+        loses = 0,
+    ): number {
         const propellerRadius = (propellerSize * 0.0254) / 2;
         const rpm = this.calculateRpm(batterySize, kv, loses);
-        return (propellerRadius * 6.28 * rpm) / 60;
+        return this.round((propellerRadius * 6.28 * rpm) / 60);
     }
 
-    calculateAcceleration(motorThrust: number, weightInGrams: number, multiplier = 4) {
-        return Math.round(((motorThrust / 1000) * 9.81 * multiplier) / (weightInGrams / 1000));
+    public calculateAcceleration(motorThrust: number, weightInGrams: number, multiplier = 4) {
+        return this.round(((motorThrust / 1000) * 9.81 * multiplier) / (weightInGrams / 1000));
+    }
+
+    private round(value: number, precision: 0 | 2 = 2): number {
+        return precision ? Math.round((value + Number.EPSILON) * 100) / 100 : Math.round(value);
     }
 }
