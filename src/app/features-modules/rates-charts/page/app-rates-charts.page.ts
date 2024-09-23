64f973ddcd50467306d03439b9c5692c43147b8e
Piotr Płaczek <piotr.placzek.91@gmail.com>
2024-09-23 09:53:23 +0200

    (#28) add actual rates chart

--- src/app/features-modules/rates-charts/page/app-rates-charts.page.ts
+++ src/app/features-modules/rates-charts/page/app-rates-charts.page.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { EChartsOption } from 'echarts';
+import { EChartsOption, SeriesOption } from 'echarts';
 import { AppRatesChartsService } from '../app-rates-charts.service';
 import { FormControl, FormGroup } from '@angular/forms';
 import { subscribeFormChanges } from '../../../shared/modules/calculator/app-calculator.utils';
+
+const SERIES_INDEX = {
+    BETAFLIGHT: 0,
+    ACTUAL: 1,
+} as const;
 
 @Component({
     templateUrl: './app-rates-charts.page.html',
@@ -11,9 +16,9 @@ import { subscribeFormChanges } from '../../../shared/modules/calculator/app-cal
 export class AppRatesChartsPage implements OnInit {
     public chartValues: Pick<EChartsOption, 'series'> = {};
     public betaflightRotationSpeed = '';
-    // public actualRotationSpeed = '';
+    public actualRotationSpeed = '';
 
-    public readonly chartOptions: EChartsOption;
+    public chartOptions: EChartsOption;
 
     public readonly betaflightRatesForm = new FormGroup({
         rate: new FormControl(1),
@@ -21,20 +26,25 @@ export class AppRatesChartsPage implements OnInit {
         expo: new FormControl(0),
     });
 
-    // public readonly actualRatesForm = new FormGroup({
-    //     centerRate: new FormControl(1),
-    //     maxRate: new FormControl(0.7),
-    //     expo: new FormControl(0),
-    // });
+    public readonly actualRatesForm = new FormGroup({
+        centerRate: new FormControl(220),
+        maxRate: new FormControl(667),
+        expo: new FormControl(0.65),
+    });
 
     constructor(private readonly service: AppRatesChartsService) {
         this.chartOptions = {
             responsive: true,
             maintainAspectRatio: false,
-            radius: [20, 180],
+            legend: {
+                show: true,
+                textStyle: {
+                    color: 'white',
+                },
+            },
             tooltip: {
                 trigger: 'axis',
-                formatter: '{c0} [deg/s]',
+                valueFormatter: (value) => `${Number(value) < 0 ? Number(value) * -1 : value} [deg/s]`,
             },
             xAxis: {
                 data: service.stickPositions,
@@ -43,53 +53,60 @@ export class AppRatesChartsPage implements OnInit {
                 },
             },
             yAxis: { splitLine: { show: false } },
-            series: {
-                name: 'Betaflight',
-                type: 'line',
-                smooth: true,
-                symbol: 'none',
-                data: [],
-            },
+            series: [
+                {
+                    name: 'Betaflight',
+                    color: 'LightSalmon',
+                    type: 'line',
+                    smooth: true,
+                    symbol: 'none',
+                    data: [],
+                },
+                {
+                    name: 'Actual',
+                    color: 'CornflowerBlue',
+                    type: 'line',
+                    smooth: true,
+                    symbol: 'none',
+                    data: [],
+                },
+            ],
         };
     }
 
     ngOnInit(): void {
-        this.updateBetaflightRatesChart();
-        subscribeFormChanges(this.betaflightRatesForm, () => this.updateBetaflightRatesChart());
-        // this.updateActualRatesChart();
-        // subscribeFormChanges(this.actualRatesForm, () => this.updateActualRatesChart());
+        this.calculateBetaflightRates();
+        subscribeFormChanges(this.betaflightRatesForm, () => this.calculateBetaflightRates());
+        this.calculateActualRates();
+        subscribeFormChanges(this.actualRatesForm, () => this.calculateActualRates());
     }
 
-    private updateBetaflightRatesChart(): void {
+    private calculateBetaflightRates(): void {
         const newValues = this.service.calculateBetaflightRates(
             this.betaflightRatesForm.controls.rate.value || 1,
             this.betaflightRatesForm.controls.superRate.value || 0.7,
             this.betaflightRatesForm.controls.expo.value || 0,
         );
         this.betaflightRotationSpeed = `${newValues.at(-1)} [deg/s]`;
-
-        this.chartValues = {
-            series: [
-                {
-                    data: newValues,
-                },
-            ],
-        };
+        this.updateChart(newValues, SERIES_INDEX.BETAFLIGHT);
     }
 
-    // private updateActualRatesChart(): void {
-    //     const newValues = this.service.calculateActualRates(
-    //         this.actualRatesForm.controls.centerRate.value || 1,
-    //         this.actualRatesForm.controls.maxRate.value || 0.7,
-    //         this.actualRatesForm.controls.expo.value || 0,
-    //     );
-    //     this.actualRotationSpeed = `${newValues.at(-1)} [deg/s]`;
-    //     this.chartValues = {
-    //         series: [
-    //             {
-    //                 data: newValues,
-    //             },
-    //         ],
-    //     };
-    // }
+    private calculateActualRates(): void {
+        const newValues = this.service.calculateActualRates(
+            this.actualRatesForm.controls.centerRate.value || 220,
+            this.actualRatesForm.controls.maxRate.value || 667,
+            this.actualRatesForm.controls.expo.value || 0.65,
+        );
+        this.actualRotationSpeed = `${newValues.at(-1)} [deg/s]`;
+        this.updateChart(newValues, SERIES_INDEX.ACTUAL);
+    }
+
+    private updateChart(newValues: number[], sIndex: number): void {
+        const actualSeries = this.chartOptions.series as SeriesOption[];
+        actualSeries[sIndex].data = newValues;
+
+        this.chartOptions = Object.assign({}, this.chartOptions, {
+            series: actualSeries,
+        });
+    }
 }
